bcf955e1ddd6a72ca3c112976ac04f5b5cece5c7
Shivansh Guleria <shivanshguleria@proton.me>
2024-09-27 16:56:33 +0000

    dshgdf

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,240 @@
 import streamlit as st
 
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# from keras.preprocessing.image import ImageDataGenerator
+from keras.applications.densenet import DenseNet121
+from keras.layers import Dense, GlobalAveragePooling2D
+from keras.models import Model
+from tensorflow.python.keras.backend import get_session as K
+
+import random
+
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow.python.keras.backend as K
+from keras.preprocessing import image
+from sklearn.metrics import roc_auc_score, roc_curve
+from tensorflow.compat.v1.logging import INFO, set_verbosity
+
+random.seed(a=None, version=2)
+
+set_verbosity(INFO)
+
+labels = ['Cardiomegaly', 'Emphysema', 'Effusion', 'Hernia', 'Infiltration', 'Mass', 'Nodule', 'Atelectasis', 'Pneumothorax', 'Pleural_Thickening', 'Pneumonia', 'Fibrosis', 'Edema', 'Consolidation']
+
+# UNQ_C3 (UNIQUE CELL IDENTIFIER, DO NOT EDIT)
+def get_weighted_loss(pos_weights, neg_weights, epsilon=1e-7):
+    """
+    Return weighted loss function given negative weights and positive weights.
+
+    Args:
+      pos_weights (np.array): array of positive weights for each class, size (num_classes)
+      neg_weights (np.array): array of negative weights for each class, size (num_classes)
+
+    Returns:
+      weighted_loss (function): weighted loss function
+    """
+    def weighted_loss(y_true, y_pred):
+        """
+        Return weighted loss value.
+
+        Args:
+            y_true (Tensor): Tensor of true labels, size is (num_examples, num_classes)
+            y_pred (Tensor): Tensor of predicted labels, size is (num_examples, num_classes)
+        Returns:
+            loss (Tensor): overall scalar loss summed across all classes
+        """
+        # initialize loss to zero
+        loss = 0.0
+
+        ### START CODE HERE (REPLACE INSTANCES OF 'None' with your code) ###
+
+        for i in range(len(pos_weights)):
+            # for each class, add average weighted loss for that class
+            loss_pos = -1 * K.mean(pos_weights[i] * y_true[:, i] * K.log(y_pred[:, i] + epsilon))
+            loss_neg = -1 * K.mean(neg_weights[i] * (1 - y_true[:, i]) * K.log(1 - y_pred[:, i] + epsilon))
+            loss += loss_pos + loss_neg
+
+        return loss
+
+        ### END CODE HERE ###
+    return weighted_loss
+
+# create the base pre-trained model
+base_model = DenseNet121(weights='./nih/densenet.hdf5', include_top=False)
+
+x = base_model.output
+
+# add a global spatial average pooling layer
+x = GlobalAveragePooling2D()(x)
+
+# and a logistic layer
+predictions = Dense(len(labels), activation="sigmoid")(x)
+pos_weights = [0.98, 0.987, 0.872, 0.998, 0.825, 0.955, 0.946, 0.894, 0.962, 0.979, 0.99, 0.986, 0.984, 0.967]
+neg_weights = [0.02, 0.013, 0.128, 0.002, 0.175, 0.045, 0.054, 0.106, 0.038, 0.021, 0.01, 0.014, 0.016, 0.033]
+model = Model(inputs=base_model.input, outputs=predictions)
+model.compile(optimizer='adam', loss=get_weighted_loss( np.array(pos_weights, dtype=np.float32), np.array(neg_weights, dtype=np.float32) ))
+model.load_weights("./nih/pretrained_model.h5")
+
+
+import tensorflow.python.keras.backend as K
+
+sess = K.get_session()
+with sess.as_default() as sess:
+    print("Test example:\n")
+    y_true = K.constant(np.array(
+        [[1, 1, 1],
+         [1, 1, 0],
+         [0, 1, 0],
+         [1, 0, 1]]
+    ))
+    print("y_true:\n")
+    print(y_true.numpy())
+
+    w_p = np.array([0.25, 0.25, 0.5])
+    w_n = np.array([0.75, 0.75, 0.5])
+    print("\nw_p:\n")
+    print(w_p)
+
+    print("\nw_n:\n")
+    print(w_n)
+
+    y_pred_1 = K.constant(0.7*np.ones(y_true.shape))
+    print("\ny_pred_1:\n")
+    print(y_pred_1.numpy())
+
+    y_pred_2 = K.constant(0.3*np.ones(y_true.shape))
+    print("\ny_pred_2:\n")
+    print(y_pred_2.numpy())
+
+    L = get_weighted_loss(w_p, w_n)
+
+    print("\nIf we weighted them correctly, we expect the two losses to be the same.")
+    print(L(y_true, y_pred_1).numpy()- L(y_true, y_pred_2).numpy())
+
+
+def get_mean_std_per_batch(image_path, df, H=320, W=320):
+    sample_data = []
+    for idx, img in enumerate(df.sample(100)["Image"].values):
+        # path = image_dir + img
+        sample_data.append(
+            np.array(image.load_img(image_path, target_size=(H, W))))
+
+    mean = np.mean(sample_data[0])
+    std = np.std(sample_data[0])
+    return mean, std
+
+
+def load_image(img, image_dir, df, preprocess=True, H=320, W=320):
+    """Load and preprocess image."""
+    img_path = image_dir + img
+    mean, std = get_mean_std_per_batch(img_path, df, H=H, W=W)
+    x = image.load_img(img_path, target_size=(H, W))
+    if preprocess:
+        x -= mean
+        x /= std
+        x = np.expand_dims(x, axis=0)
+    return x
+
+
+# def grad_cam(input_model, image, cls, layer_name, H=320, W=320):
+#     """GradCAM method for visualizing input saliency."""
+#     y_c = input_model.output[0, cls]
+#     conv_output = input_model.get_layer(layer_name).output
+#     grads = K.gradients(y_c, conv_output)[0]
+
+#     gradient_function = K.function([input_model.input], [conv_output, grads])
+
+#     output, grads_val = gradient_function([image])
+#     output, grads_val = output[0, :], grads_val[0, :, :, :]
+
+#     weights = np.mean(grads_val, axis=(0, 1))
+#     cam = np.dot(output, weights)
+
+#     # Process CAM
+#     cam = cv2.resize(cam, (W, H), cv2.INTER_LINEAR)
+#     cam = np.maximum(cam, 0)
+#     cam = cam / cam.max()
+#     return cam
+
+
+# def compute_gradcam(model, img, image_dir, df, labels, selected_labels,
+#                     layer_name='bn'):
+#     preprocessed_input = load_image(img, image_dir, df)
+#     predictions = model.predict(preprocessed_input)
+
+#     print("Loading original image")
+#     plt.figure(figsize=(15, 10))
+#     plt.subplot(151)
+#     plt.title("Original")
+#     plt.axis('off')
+#     plt.imshow(load_image(img, image_dir, df, preprocess=False), cmap='gray')
+
+#     j = 1
+#     for i in range(len(labels)):
+#         if labels[i] in selected_labels:
+#             print(f"Generating gradcam for class {labels[i]}")
+#             gradcam = grad_cam(model, preprocessed_input, i, layer_name)
+#             plt.subplot(151 + j)
+#             plt.title(f"{labels[i]}: p={predictions[0][i]:.3f}")
+#             plt.axis('off')
+#             plt.imshow(load_image(img, image_dir, df, preprocess=False),
+#                        cmap='gray')
+#             plt.imshow(gradcam, cmap='jet', alpha=min(0.5, predictions[0][i]))
+            # j += 1
+
+
+def get_roc_curve(labels, predicted_vals, generator):
+    auc_roc_vals = []
+    for i in range(len(labels)):
+        try:
+            gt = generator.labels[:, i]
+            pred = predicted_vals[:, i]
+            auc_roc = roc_auc_score(gt, pred)
+            auc_roc_vals.append(auc_roc)
+            fpr_rf, tpr_rf, _ = roc_curve(gt, pred)
+            plt.figure(1, figsize=(10, 10))
+            plt.plot([0, 1], [0, 1], 'k--')
+            plt.plot(fpr_rf, tpr_rf,
+                     label=labels[i] + " (" + str(round(auc_roc, 3)) + ")")
+            plt.xlabel('False positive rate')
+            plt.ylabel('True positive rate')
+            plt.title('ROC curve')
+            plt.legend(loc='best')
+        except:
+            print(
+                f"Error in generating ROC curve for {labels[i]}. "
+                f"Dataset lacks enough examples."
+            )
+    plt.show()
+    return auc_roc_vals
+df = pd.read_csv("nih/train-small.csv")
+preprocessed_input = load_image("1.jpeg", "nih/", df)
+predictions = model.predict(preprocessed_input)
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    # To read file as bytes:
+    bytes_data = uploaded_file.getvalue()
+    st.write(bytes_data)
+
+    # To convert to a string based IO:
+    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    st.write(stringio)
+
+    # To read file as string:
+    string_data = stringio.read()
+    st.write(string_data)
+
+    # Can be used wherever a "file-like" object is accepted:
+    dataframe = pd.read_csv(uploaded_file)
+    st.write(dataframe)
